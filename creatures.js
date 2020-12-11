e35a22fdc1686adcd90f571aa0c01b39b3563b47
qlovelace <105613099+qlovelace@users.noreply.github.com>
2020-12-11 18:17:31 +0000

    Error message on form.

--- creatures.js
+++ creatures.js
@@ -108,3 +108,128 @@ class Mob extends Creature {
         return [0, 0];
     }
 }
+
+class Orc extends Mob {
+    constructor(startX, startY, weaponID, armorID) {
+        super(79);
+        this.name = "Green orc";
+        this.strength = rollDice(6, 3) + 2 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.agility = rollDice(6, 3) - 4 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.endurance = rollDice(6, 3) + 1 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.attack = Math.floor(this.strength / 10);
+        this.x = startX;
+        this.y = startY;
+        this.hp = Math.floor(this.endurance / 10) + 4;
+        this.initiative = this.agility / 10 + 8;
+        this.gold = Math.floor(Math.random() * (17 + depth)) + 3;
+        this.weapon = weapons[weaponID];
+        this.armor = armor[armorID];
+        this.color = "green";
+        this.left = 1;
+    }
+
+    move(map) {
+        let lastx = this.x;
+        let lasty = this.y;
+        if (this.left === 1) {
+            if (!map[this.y][this.x - 1].isMovable) {
+                this.left = 0;
+            } else {
+                this.x--;
+            }
+        } else {
+            if (!map[this.y][this.x + 1].isMovable) {
+                this.left = 1;
+            } else {
+                this.x++;
+            }
+        }
+        return [lastx, lasty];
+    }
+}
+
+class Kobold extends Mob {
+    constructor(startX, startY, weaponID, armorID) {
+        super(75);
+        this.name = "little kobold";
+        this.strength = rollDice(6, 3) - 5 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.agility = rollDice(6, 3) + 3 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.endurance = rollDice(6, 3) - 5 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.attack = Math.floor(this.strength / 10);
+        this.x = startX;
+        this.y = startY;
+        this.hp = Math.floor(this.endurance / 10) + 4;
+        this.initiative = this.agility / 10 + 8;
+        this.gold = Math.floor(Math.random() * (13 + depth)) + 3;
+        this.weapon = weapons[weaponID];
+        this.armor = armor[armorID];
+        this.color = "violet";
+        this.up = 1;
+    }
+
+    move(map) {
+        let lastx = this.x;
+        let lasty = this.y;
+        if (this.up === 1) {
+            if (!map[this.y - 1][this.x].isMovable) {
+                this.up = 0;
+            } else {
+                this.y--;
+            }
+        } else {
+            if (!map[this.y + 1][this.x].isMovable) {
+                this.up = 1;
+            } else {
+                this.y++;
+            }
+        }
+        return [lastx, lasty];
+    }
+}
+
+class Gargoyle extends Mob {
+    constructor(startX, startY, weaponID, armorID) {
+        super(71);
+        this.name = "Stone Gargoyle";
+        this.strength = rollDice(6, 3) + 2 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.agility = rollDice(6, 3) + 1 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.endurance = rollDice(6, 3) + 5 + Math.floor(Math.random() * Math.floor(depth / 2));
+        this.attack = Math.floor(this.strength / 10);
+        this.x = startX;
+        this.y = startY;
+        this.hp = Math.floor(this.endurance / 10) + 4;
+        this.initiative = this.agility / 10 + 8;
+        this.gold = Math.floor(Math.random() * (20 + depth)) + 3;
+        this.weapon = weapons[weaponID];
+        this.armor = armor[armorID];
+        this.color = "#FFB459";
+        this.leftUp = 1;
+    }
+
+    move(map) {
+        let lastx = this.x;
+        let lasty = this.y;
+        if (this.leftUp === 1) {
+            if (!map[this.y - 1][this.x - 1].isMovable) {
+                this.leftUp = 0;
+            } else {
+                this.y--;
+                this.x--;
+            }
+        } else {
+            if (!map[this.y + 1][this.x + 1].isMovable) {
+                this.leftUp = 1;
+            } else {
+                this.y++;
+                this.x++;
+            }
+        }
+        return [lastx, lasty];
+    }
+}
+
+class Minotaur extends Mob {
+    constructor(startX, startY, weaponID, armorID) {
+        super(77);
+        this.name = "Blind minotaur";
+        this.strength = rollDice(6, 3) + 30 + Math.floor(Math.random() * Math.floor(depth / 2));
