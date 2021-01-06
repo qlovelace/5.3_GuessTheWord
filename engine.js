0c9f1c6ad4a6d4a4655b40d14a6ea4c1550eafb0
qlovelace <105613099+qlovelace@users.noreply.github.com>
2021-01-06 15:20:11 +0000

    Composer init

--- engine.js
+++ engine.js
@@ -2,3 +2,6 @@ let game = null;//new GameState();
 let menu = null;
 let credits = null;
 let leaderboards = null;
+let settings = null;
+let gameOver = null;//new GameOverState();
+let charCreation = 
