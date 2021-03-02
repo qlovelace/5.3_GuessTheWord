04127a687b8498de9840c0d4e7627ef70b822257
qlovelace <105613099+qlovelace@users.noreply.github.com>
2021-03-02 06:08:05 +0000

    [maven-release-plugin] prepare release 1.0.3

--- engine.js
+++ engine.js
@@ -320,4 +320,5 @@ class GameOver extends State {
         game.drawMessage(game.messages[0], 30, 505 - 100, context);      //0
         game.drawMessage(game.messages[1], 30, 525 - 100 - 1, context);      //1
         game.drawMessage(game.messages[2], 30, 545 - 100 - 2, context);      //2
-     
+        game.drawMessage(game.messages[3], 30, 565 - 100 - 3, context);      //3
+        game.d
