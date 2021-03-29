a9268e3082e935b372f1cd12d01f2564d8c8a4ff
qlovelace <105613099+qlovelace@users.noreply.github.com>
2021-03-29 21:49:36 +0000

    add github

--- engine.js
+++ engine.js
@@ -461,4 +461,5 @@ class CharCreationState extends State {
 
     keyHandler(scene, event) {
         switch (event.keyCode) {
-            case 72: 
+            case 72: //h
+                mainHero = new Player('Human', 10, 10, dbUser ? dbUser.di
