2e015ab51d5e987a4d0162f3c0fd8afe05151052
qlovelace <105613099+qlovelace@users.noreply.github.com>
2022-09-17 14:09:21 +0000

    path change

--- items.js
+++ items.js
@@ -23,4 +23,10 @@ class Armor {
 
 class PoorClothes extends Armor {
     constructor() {
-        super('Poor clothes', 2, 'It smells like your cat!', 0
+        super('Poor clothes', 2, 'It smells like your cat!', 0);
+    }
+}
+
+class LeatherPants extends Armor {
+    constructor() {
+        super('Leather pa
