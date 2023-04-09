c3a34288796711764e083d23c0fb42d8ff5db059
qlovelace <105613099+qlovelace@users.noreply.github.com>
2023-04-09 11:38:29 +0000

    Fix: Run 'make cs'

--- rollDice.js
+++ rollDice.js
@@ -27,3 +27,89 @@ function getRandomPotion() {
 }
 
 function incStat(a) {
+    if (mainHero.gold >= statCost) {
+        switch (a){
+            case 1: mainHero.clearStrength++;
+                        break;
+            case 2: mainHero.clearAgility++;
+                            break;
+            case 3: mainHero.clearEndur++;
+                              break;
+            case 4: mainHero.clearInt++;
+                              break;
+        }
+        mainHero.update();
+        mainHero.gold -= statCost;
+        statCost += 25;
+        return true;
+    }
+    return false;
+}
+
+function getRandomWeapon(tier) {
+    if (tier > maxTier) {
+        return false;
+    }
+
+    if (mainHero.weapon.tier === tier) {
+        if (mainHero.gold < weaponCostSame) return false;
+
+        let choice = [];
+
+        for (let i = 0; i < weapons.length; i++)
+            if (weapons[i].tier === tier) choice.push(weapons[i]);
+
+        mainHero.weapon.onChange(mainHero);
+        mainHero.weapon = choice[Math.floor(Math.random() * choice.length)];
+        mainHero.weapon.onEquip(mainHero);
+        mainHero.update();
+        mainHero.gold -= weaponCostSame;
+        weaponCostSame += 10;
+        return true;
+    }
+    if (mainHero.gold < weaponCostUp) return false;
+
+    let choice = [];
+
+    for (let i = 0; i < weapons.length; i++)
+        if (weapons[i].tier === tier) choice.push(weapons[i]);
+
+    mainHero.weapon.onChange(mainHero);
+    mainHero.weapon = choice[Math.floor(Math.random() * choice.length)];
+    mainHero.weapon.onEquip(mainHero);
+    mainHero.update();
+    mainHero.gold -= weaponCostUp;
+    weaponCostUp += 150;
+    return true;
+
+}
+
+function getRandomArmor(tier) {
+    if (tier > maxTier) {
+        return false;
+    }
+
+    if (mainHero.armor.tier === tier) {
+        if (mainHero.gold < armorCostSame) return false;
+
+        let choice = [];
+
+        for (let i = 0; i < armor.length; i++)
+            if (armor[i].tier === tier) choice.push(armor[i]);
+
+        mainHero.armor.onChange(mainHero);
+        mainHero.armor = choice[Math.floor(Math.random() * choice.length)];
+        mainHero.armor.onEquip(mainHero);
+        mainHero.update();
+        mainHero.gold -= armorCostSame;
+        armorCostSame += 10;
+        return true;
+    }
+    if (mainHero.gold < armorCostUp) return false;
+
+    let choice = [];
+
+    for (let i = 0; i < armor.length; i++)
+        if (armor[i].tier === tier) choice.push(armor[i]);
+
+    mainHero.armor.onChange(mainHero);
