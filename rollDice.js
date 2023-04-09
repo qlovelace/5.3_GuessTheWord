5eda9f4fb5ae83f505621add5bcc2a5430a4fee7
qlovelace <105613099+qlovelace@users.noreply.github.com>
2023-04-09 12:32:50 +0000

    Removed debug prints

--- rollDice.js
+++ rollDice.js
@@ -113,3 +113,109 @@ function getRandomArmor(tier) {
         if (armor[i].tier === tier) choice.push(armor[i]);
 
     mainHero.armor.onChange(mainHero);
+    mainHero.armor = choice[Math.floor(Math.random() * choice.length)];
+    mainHero.armor.onEquip(mainHero);
+    mainHero.update();
+    mainHero.gold -= armorCostUp;
+    armorCostUp += 150;
+    return true;
+
+}
+
+function getRandomMagic(){
+    if (mainHero.gold >= magicCost) {
+        mainHero.magic = spells[Math.floor(Math.random()*spells.length)];
+        mainHero.update();
+        mainHero.gold -= magicCost;
+        magicCost += 50;
+        return true;
+    }
+    return false;
+}
+
+function closeBattle(first, second) {
+    if (first.isDead || second.isDead)
+        return;
+
+    game.pushMessage(`(${first.name}){${first.color}}( bumped with ){white}(${second.name}){${second.color}}( !){white}`);
+    if (first.agility / 10 + rollDice(6, 2) > 6 + second.agility / 10) {
+        let damage = rollDice(first.weapon.diceVal, first.weapon.diceCount) - second.armor.value + first.attack;
+        let crit = (rollDice(20, 1) > 18) && (((first.weapon.diceVal - 1) * first.weapon.diceCount) <= damage);
+        crit ? damage += damage : damage;
+        damage > 0 ? (() => {
+                second.hp -= damage;
+                game.pushMessage(
+                    `(${first.name} ){${first.color}}(attacked ){white}(${second.name} )` +
+                    `{${second.color}}(with his ${first.weapon.name} for ` +
+                    `${damage} damage! ){white}${(crit ? '(crit!){red}' : ' ')}`);
+            })() :
+            (() => {
+                damage = 0;
+                game.pushMessage(`(${first.name} ){${first.color}}` +
+                                `(did not even scratch ){white}` +
+                                `(${second.name}){${second.color}}(!){white}`)
+            })();
+        if (second.hp <= 0) {
+            game.pushMessage(`(${second.name}){${second.color}}( is dead!){red}`);
+            first.gold += second.gold;
+            game.pushMessage(`(${first.name} gained ){white}(${second.gold}){#ffd700}( gold!){white}`);
+            let potionRoll = rollDice(20,1);
+            if(potionRoll > 18 && !mainHero.isDead) {
+                mainHero.hpPotions++;
+                game.pushMessage(`(You have found){white} (a health potion){green}`)
+            }
+            second.isDead = 1;
+            return;
+        }
+    } else {
+        game.pushMessage(`(${first.name}){${first.color}}( missed ){green}(${second.name}){${second.color}}( !){green}`);
+    }
+    if (second.agility / 10 + rollDice(6, 2) > 6 + first.agility / 10) {
+        let damage = rollDice(second.weapon.diceVal, second.weapon.diceCount) - first.armor.value + second.attack;
+        let crit = (rollDice(20, 1) > 18) && (((second.weapon.diceVal - 1) * second.weapon.diceCount) <= damage);
+        crit ? damage += damage : damage;
+        damage > 0 ? (() => {
+                first.hp -= damage;
+                game.pushMessage(
+                    `(${second.name}){${second.color}}( attacked ){white}` +
+                    `(${first.name}){${first.color}}( with his ${second.weapon.name} for ){white}` +
+                    `(${damage} damage! ){white}${(crit ? '(crit!){red}' : ' ')}`);
+            })() :
+            (() => {
+                damage = 0;
+                game.pushMessage(`(${second.name}){${second.color}}` +
+                                 `( did not hurt ){white}` +
+                                 `(${first.name}){${first.color}}(!){white}`)
+            })();
+        if (first.hp <= 0) {
+            game.pushMessage(`(${first.name} ){${first.color}}(is dead!){red}`);
+            second.gold += first.gold;
+            game.pushMessage(`(${second.name} gained ){white}(${first.gold}){#ffd700}( gold!){white}`);
+            let potionRoll = rollDice(20,1);
+            if(potionRoll > 18 && !mainHero.isDead) {
+                mainHero.hpPotions++;
+                game.pushMessage(`(You have found){white} (a health potion){green}`)
+            }
+            first.isDead = 1;
+            return;
+        }
+    } else {
+        game.pushMessage(`(${second.name}){${second.color}}( missed ){green}(${first.name}){${first.color}}( !){green}`);
+    }
+}
+
+
+function mobCloseBattle(first, second) {
+    if (first.isDead || second.isDead)
+        return;
+
+    if (first.agility / 10 + rollDice(6, 2) > 6 + second.agility / 10) {
+        let damage = rollDice(first.weapon.diceVal, first.weapon.diceCount) - second.armor.value + first.attack;
+        let crit = (rollDice(20, 1) > 18) && (((first.weapon.diceVal - 1) * first.weapon.diceCount) <= damage);
+        crit ? damage += damage : damage;
+        damage > 0 ? (() => {
+                second.hp -= damage;
+
+            })() :
+            (() => {
+                damage = 0;
